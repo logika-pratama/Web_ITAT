b751756027e134fa5579135a6076b6ff90e367ea
satrio singgih <satriosinggihwicaksono@gmail.com>
2023-05-08 22:35:14 +0700

    add

--- application/views/login.php
+++ application/views/login.php
@@ -1,48 +1,17 @@
 <!DOCTYPE html>
-
-<!-- =========================================================
-* Sneat - Bootstrap 5 HTML Admin Template - Pro | v1.0.0
-==============================================================
-
-* Product Page: https://themeselection.com/products/sneat-bootstrap-html-admin-template/
-* Created by: ThemeSelection
-* License: You must have a valid license purchased in order to legally use the theme for your project.
-* Copyright ThemeSelection (https://themeselection.com)
-
-=========================================================
- -->
-<!-- beautify ignore:start -->
-<html
-  lang="en"
-  class="light-style customizer-hide"
-  dir="ltr"
-  data-theme="theme-default"
-  data-assets-path="<?=base_url()?>assets/"
-  data-template="vertical-menu-template-free"
->
+<html lang="en">
   <head>
     <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
-    />
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
     <title>ITAT Mobile</title>
-
     <meta name="description" content="" />
     <link rel="icon" type="image/x-icon" href="<?=base_url()?>assets/img/favicon/favicon.ico" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet"
-    />
     <link rel="stylesheet" href="<?=base_url()?>assets/vendor/fonts/boxicons.css" />
     <link rel="stylesheet" href="<?=base_url()?>assets/vendor/css/core.css" class="template-customizer-core-css" />
     <link rel="stylesheet" href="<?=base_url()?>assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
     <link rel="stylesheet" href="<?=base_url()?>assets/css/demo.css" />
     <link rel="stylesheet" href="<?=base_url()?>assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.7.5/sweetalert2.min.css"/>
+    <link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.7.5/sweetalert2.min.css"/>
     <link rel="stylesheet" href="<?=base_url()?>assets/vendor/css/pages/page-auth.css" />
     <script src="<?=base_url()?>assets/vendor/js/helpers.js"></script>
     <script src="<?=base_url()?>assets/js/config.js"></script>
@@ -156,7 +125,6 @@
         </div>
       </div>
     </div>
-
     <!-- / Content -->
 
     <!-- Core JS -->
@@ -224,11 +192,8 @@
     $(document).ready(function() {
       setTimeout(function() {
         $('#ctn-preloader').addClass('loaded');
-        // Una vez haya terminado el preloader aparezca el scroll
         $('body').removeClass('no-scroll-y');
-
         if ($('#ctn-preloader').hasClass('loaded')) {
-          // Es para que una vez que se haya ido el preloader se elimine toda la seccion preloader
           $('#preloader').delay(1000).queue(function() {
             $(this).remove();
           });
--- application/views/template.php
+++ application/views/template.php
@@ -1,60 +1,22 @@
 <!DOCTYPE html>
-
-<!-- =========================================================
-* Sneat - Bootstrap 5 HTML Admin Template - Pro | v1.0.0
-==============================================================
-
-* Product Page: https://themeselection.com/products/sneat-bootstrap-html-admin-template/
-* Created by: ThemeSelection
-* License: You must have a valid license purchased in order to legally use the theme for your project.
-* Copyright ThemeSelection (https://themeselection.com)
-
-=========================================================
- -->
-<!-- beautify ignore:start -->
-<html
-  lang="en"
-  class="light-style layout-menu-fixed"
-  dir="ltr"
-  data-theme="theme-default"
-  data-assets-path="<?=base_url()?>assets/"
-  data-template="vertical-menu-template-free"
->
+<html lang="en">
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"/>
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>ITAT Mobile</title>
-
     <meta name="description" content="" />
-
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="<?=base_url()?>assets/img/favicon/favicon.ico" />
-
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet"
-    />
-
-    <!-- Icons. Uncomment required icon fonts -->
     <link rel="stylesheet" href="<?=base_url()?>assets/vendor/fonts/boxicons.css" />
-
-    <!-- Core CSS -->
     <link rel="stylesheet" href="<?=base_url()?>assets/vendor/css/core.css" class="template-customizer-core-css" />
     <link rel="stylesheet" href="<?=base_url()?>assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
     <link rel="stylesheet" href="<?=base_url()?>assets/css/demo.css" />
-
-    <!-- Vendors CSS -->
     <link rel="stylesheet" href="<?=base_url()?>assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-
     <link rel="stylesheet" href="<?=base_url()?>assets/vendor/libs/apex-charts/apex-charts.css" />
-
-    <!-- Page CSS -->
-
-    <!-- Helpers -->
     <script src="<?=base_url()?>assets/vendor/js/helpers.js"></script>
     <script src="<?=base_url()?>assets/js/config.js"></script>
   </head>
@@ -110,24 +72,13 @@
     </div>
     <!-- / Layout wrapper -->
    
-    <!-- Core JS -->
-    <!-- build:js assets/vendor/js/core.js -->
-
     <script src="<?=base_url()?>assets/vendor/libs/jquery/jquery.js"></script>
     <script src="<?=base_url()?>assets/vendor/libs/popper/popper.js"></script>
     <script src="<?=base_url()?>assets/vendor/js/bootstrap.js"></script>
     <script src="<?=base_url()?>assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-
     <script src="<?=base_url()?>assets/vendor/js/menu.js"></script>
-    <!-- endbuild -->
-
-    <!-- Vendors JS -->
     <script src="<?=base_url()?>assets/vendor/libs/apex-charts/apexcharts.js"></script>
-
-    <!-- Main JS -->
     <script src="<?=base_url()?>assets/js/main.js"></script>
-
-    <!-- Page JS -->
     <script src="<?=base_url()?>assets/js/dashboards-analytics.js"></script>
     <script>
       function changePage(){
