d72c340fb2b82b7c4952153677aca043f7edb752
satrio singgih <satriosinggihwicaksono@gmail.com>
2023-06-12 13:23:36 +0700

    add

--- application/controllers/Scan_rfid.php
+++ application/controllers/Scan_rfid.php
@@ -53,17 +53,19 @@ class Scan_rfid extends CI_Controller {
 			$response = curl_exec($curl);
 			curl_close($curl);
 			$rss = json_decode($response);
-			if($rss->meta->message != 'Asset tidak ditemukan'){
-				if($rss->meta->status == 'success'){
-					$brr[$x]['assets_id'] = $rss->data[0]->asset_id;
-					$brr[$x]['location_asset'] = $rss->data[0]->location_asset;
-					if(!empty($rss->data[0]->name_asset)){
-						$brr[$x]['name_asset'] = $rss->data[0]->name_asset;
-					} else {
-						$brr[$x]['name_asset'] = '';
-					}
-					$x++;
-				} 
+			if(!empty($rss)){
+				if($rss->meta->message != 'Asset tidak ditemukan'){
+					if($rss->meta->status == 'success'){
+						$brr[$x]['assets_id'] = $rss->data[0]->asset_id;
+						$brr[$x]['location_asset'] = $rss->data[0]->location_asset;
+						if(!empty($rss->data[0]->name_asset)){
+							$brr[$x]['name_asset'] = $rss->data[0]->name_asset;
+						} else {
+							$brr[$x]['name_asset'] = '';
+						}
+						$x++;
+					} 
+				}
 			}
 		}
 	
