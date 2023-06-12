39dfd805f43ce2283ed1c1b60ba7f93a5ac5daeb
satrio singgih <satriosinggihwicaksono@gmail.com>
2023-06-12 11:38:16 +0700

    add

--- application/controllers/Pindai_rfid.php
+++ application/controllers/Pindai_rfid.php
@@ -37,7 +37,7 @@ class Pindai_rfid extends CI_Controller {
 
 			$curl = curl_init();
 			curl_setopt_array($curl, array(
-			CURLOPT_URL => 'https://10.230.200.158:8082/api/ujimat/set_view_ujimat?asset_id='.$a,
+			CURLOPT_URL => 'http://10.230.200.158:8081/api/gate_in',
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_ENCODING => '',
 			CURLOPT_MAXREDIRS => 10,
@@ -45,17 +45,20 @@ class Pindai_rfid extends CI_Controller {
 			CURLOPT_FOLLOWLOCATION => true,
 			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 			CURLOPT_CUSTOMREQUEST => 'POST',
+			CURLOPT_POSTFIELDS =>'[
+				{
+					"rfid_code": "'.$a.'"
+				}
+			]',
 			CURLOPT_HTTPHEADER => array(
+				'Content-Type: application/json',
 				'apikey: $pbkdf2-sha512$6000$P4cQYmzN.X8v5bw3xhijtA$PzGUd4dnuuvvEDgwhUsvDafEKu4W4Z5McvDO5nchfAlllfNsbCXBeB5XE/KrbtFEqfM4ymR2IMzGsKWT0vXKFA'
 			),
 			));
-
 			$response = curl_exec($curl);
 			curl_close($curl);
 			$rss = json_decode($response);
-
 			if($rss->meta->status != 'success'){
-
 				$a = str_replace(" ","",$a);
 				$curl = curl_init();
 				curl_setopt_array($curl, array(
