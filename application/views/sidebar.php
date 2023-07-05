889297ee10c45334aabbf81d9573815c8e834a1e
Muhammad Alfat Furqon <muhammadalfat.ma@gmail.com>
2023-07-05 09:56:46 +0700

    Mengubah nama Uji Mat (QR Code) menjadi Uji Mat

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -84,7 +84,8 @@
         </li>
         <li class="menu-item">
             <a href="javascript:void(0)" onclick="changePage()" data-url="<?=base_url('index.php/ujimat_scanqr')?>"  class="menu-link">
-            <div data-i18n="Container">Uji Mat (QR Code)</div>
+            <!-- <div data-i18n="Container">Uji Mat (QR Code)</div> -->
+            <div data-i18n="Container">Uji Mat</div>
             </a>
         </li>
         <!-- <li class="menu-item">
