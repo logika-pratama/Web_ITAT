15d9776ced0282af5a6fdf67d35e5b5b815e3ec2
satrio singgih <satriosinggihwicaksono@gmail.com>
2023-07-06 14:33:47 +0700

    add

--- application/controllers/Dashboard_ujimat.php
+++ application/controllers/Dashboard_ujimat.php
@@ -14,5 +14,70 @@ class Dashboard_ujimat extends CI_Controller {
 	{
 		$this->load->view('dashboard');
 	}
+
+	public function addItem($rfid,$status){
+		if($status == 1){
+			$rss = $this->setRFID($rfid);
+			$data = array(
+				'rfid' => $rfid,
+				'rss' => $rss,
+			);
+			$this->db->insert('ujimat',$data);
+
+		} else {
+			$this->db->where('rfid',$rfid);
+			$this->db->delete('ujimat');
+		}
+
+		curl_close($curl);
+		$rss = json_decode($response,true);
+		echo json_encode($rss['data']);
+
+		$data = array(
+			'status_ujimat' => 0,
+		);
+		$this->db->update('kontrak',$data);
+	}
+
+	public function getStatus(){
+		$data = $this->db->get('kontrak')->row_array();
+		echo json_encode($data);
+	}
+
+	public function getUjimat(){
+		$this->db->order_by('id','desc');
+		$rs = $this->db->get('ujimat')->result_array();
+		?>
+		 <table style="width:100%;">
+			<tr>
+				<th>RFID</th>
+				<th>Nama Aset</th>
+				<th>Detail</th>
+			<tr>
+		<?php 
+		
+			foreach($rs as $r){
+			$data = json_decode($r['rss'],true);
+			$produk = $data['data']['detail'][0]['product_attribute'];
+		?>
+			<tr>
+				<td><?=$r['rfid']?></td>
+				<td><?=$data['data']['detail'][0]['name_asset']?></td>
+				<td>
+					<div class="row">
+						<?php 
+							foreach($produk as $p){
+						?> 
+						<div class="col-6"><?=$p['name']?></div>
+						<div class="col-6"><?=$p['description']?></div>
+						<?php } ?>
+					</div>
+				</td>
+			</tr>
+			<?php } ?>
+		 </table>
+		<?php
+
+	}
 	
 }
