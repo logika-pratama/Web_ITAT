87f37abf224a164615f50135c719237e6591cbf3
Muhammad Alfat Furqon <muhammadalfat.ma@gmail.com>
2023-07-06 09:13:26 +0700

    Fix notif 20 detik

--- application/views/ujimat_scanqr/footer.php
+++ application/views/ujimat_scanqr/footer.php
@@ -30,7 +30,7 @@ const Toast = Swal.mixin({
     toast: true,
     position: 'top-end',
     showConfirmButton: false,
-    timer: 20000,
+    timer: 2000,
     timerProgressBar: true,
     didOpen: (toast) => {
         toast.addEventListener('mouseenter', Swal.stopTimer)
