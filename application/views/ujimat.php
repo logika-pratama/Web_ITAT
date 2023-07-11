47e571b07b8fc494494614ccd5da432ce3701568
satrio singgih <satriosinggihwicaksono@gmail.com>
2023-07-11 14:07:41 +0700

    add

--- application/views/ujimat.php
+++ application/views/ujimat.php
@@ -253,7 +253,7 @@
               $(".list-data").html('');
               $('.asset_id').text(data['data'][0]['asset_id']);
               $('.name_asset').text(data['data'][0]['name_asset']);
-              $('.price').text(data['data'][0]['price']);
+              $('.price').text(formatPriceToIDR(data['data'][0]['price']));
               $('.serial_number').text(data['data'][0]['serial_number']);
               $('.year_project').text(data['data'][0]['year_project']);
               $('.ppk_user').text(data['data'][0]['ppk_user']);
@@ -314,5 +314,18 @@
           },
       });
     }
+
+    function formatPriceToIDR(value) {
+      if (!value) return value;
+      let valueStr = String(value);
+      if (valueStr.length <= 3) return 'Rp'+valueStr
+      let startPos = valueStr.length % 3;
+      let result = valueStr.slice(0, startPos) + '.' + valueStr.slice(startPos).match(/\d{3}/g).join('.');
+      if (result[0] == '.') {
+          result = result.substr(1)
+      }
+      return 'Rp'+result;
+  }
+
   </script>
 </html>
