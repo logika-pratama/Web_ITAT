b139be8daf359b09659aa94600d01e243b48c8ca
Muhammad Alfat Furqon <muhammadalfat.ma@gmail.com>
2023-07-13 10:10:26 +0700

    Fix cam

--- application/views/ujimat.php
+++ application/views/ujimat.php
@@ -296,40 +296,44 @@
           success: function(data){
             // console.log(data)
             // $('.scan').hide();
-
-
-            $('.asset_id').text(data['data']['detail'][0]['asset_id']);
-            $('.name_asset').text(data['data']['detail'][0]['name_asset']);
-            $('.price').text(formatPriceToIDR(data['data']['detail'][0]['price']));
-            $('.serial_number').text(data['data']['detail'][0]['serial_number']);
-            $('.year_project').text(data['data']['detail'][0]['year_project']);
-            $('.ppk_user').text(data['data']['detail'][0]['ppk_user']);
-            $('.name_project').text(data['data']['detail'][0]['name_project']);
-            $('.name_vendor').text(data['data']['detail'][0]['name_vendor']);
-
-            // var i;
-
-            for (let i = 0; i < data['data']['detail'][0]['product_attribute'].length; ++i) {
-                if(data['data']['detail'][0]['product_attribute'][i]['description'] != ""){
-                    $('.list-data').append("<tr><td>"+data['data']['detail'][0]['product_attribute'][i]['name']+"</td><td>"+data['data']['detail'][0]['product_attribute'][i]['description']+"</td></tr>");
+            if (!data?.data?.detail[0]?.asset_id) {
+                Toast.fire({
+                    icon: 'error',
+                    title: 'Request data dari ITAM gagal, scan ulang'
+                })
+                $('.scan').show();
+            } else {
+                $('.asset_id').text(data['data']['detail'][0]['asset_id']);
+                $('.name_asset').text(data['data']['detail'][0]['name_asset']);
+                $('.price').text(formatPriceToIDR(data['data']['detail'][0]['price']));
+                $('.serial_number').text(data['data']['detail'][0]['serial_number']);
+                $('.year_project').text(data['data']['detail'][0]['year_project']);
+                $('.ppk_user').text(data['data']['detail'][0]['ppk_user']);
+                $('.name_project').text(data['data']['detail'][0]['name_project']);
+                $('.name_vendor').text(data['data']['detail'][0]['name_vendor']);
+    
+                // var i;
+    
+                for (let i = 0; i < data['data']['detail'][0]['product_attribute'].length; ++i) {
+                    if(data['data']['detail'][0]['product_attribute'][i]['description'] != ""){
+                        $('.list-data').append("<tr><td>"+data['data']['detail'][0]['product_attribute'][i]['name']+"</td><td>"+data['data']['detail'][0]['product_attribute'][i]['description']+"</td></tr>");
+                    }
                 }
-            }
-
-            // console.log(data.data.move)
-            for (let z = 0; z < data['data']['move'].length; ++z) {
-                // console.log(data['data']['move'][z])
-                var todaydate = new Date(data['data']['move'][z]['tanggal']); 
-                var dd = todaydate .getDate();
-                var mm = todaydate .getMonth()+1;
-                var yyyy = todaydate .getFullYear();
-                if(dd<10){  dd='0'+dd } 
-                if(mm<10){  mm='0'+mm } 
-                var date = dd+'-'+mm+'-'+yyyy+' '+todaydate.getHours() + ':' + todaydate.getMinutes();
-
-                $('.list-data-history').append("<tr><td>"+data['data']['move'][z]['location_awal']+"</td><td>"+data['data']['move'][z]['location_tujuan']+"</td><td>"+date+"</td></tr>");
-            }
-
-            if (data?.data?.detail[0]?.asset_id) {
+    
+                // console.log(data.data.move)
+                for (let z = 0; z < data['data']['move'].length; ++z) {
+                    // console.log(data['data']['move'][z])
+                    var todaydate = new Date(data['data']['move'][z]['tanggal']); 
+                    var dd = todaydate .getDate();
+                    var mm = todaydate .getMonth()+1;
+                    var yyyy = todaydate .getFullYear();
+                    if(dd<10){  dd='0'+dd } 
+                    if(mm<10){  mm='0'+mm } 
+                    var date = dd+'-'+mm+'-'+yyyy+' '+todaydate.getHours() + ':' + todaydate.getMinutes();
+    
+                    $('.list-data-history').append("<tr><td>"+data['data']['move'][z]['location_awal']+"</td><td>"+data['data']['move'][z]['location_tujuan']+"</td><td>"+date+"</td></tr>");
+                }
+    
                 $('#modalLong').modal('show');
             }
 
