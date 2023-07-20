413fdadddfd51685b91439d279ac390d9ed694fc
Muhammad Alfat Furqon <muhammadalfat.ma@gmail.com>
2023-07-20 14:06:20 +0700

    Fix naming in dashboard

--- application/views/template.php
+++ application/views/template.php
@@ -84,22 +84,28 @@
                                                     <p class="icon-name text-capitalize">Scan RFID</p>
                                                 </a>
                                             </div>
-                                            <div class="col-4">
+                                            <!-- <div class="col-4">
                                                 <a href="javascript:void(0)" onclick="changePageMain()" data-url="<?=base_url('index.php/ujimat')?>" class="btn btn-secondary btn-sm mb-2" style="height:107px; width:100%;"  class="btn btn-primary btn-sm mb-2" style="height:85px; width:100%;">
                                                     <i class="bx bx-package bx-sm mb-1 mt-2"></i>
                                                     <p class="icon-name text-capitalize">Uji Mat CAM (QR Code)</p>
                                                 </a>
-                                            </div>
-                                            <div class="col-4">
+                                            </div> -->
+                                            <!-- <div class="col-4">
                                                 <a href="javascript:void(0)" onclick="changePageMain()" data-url="<?=base_url('index.php/ujimatrfid')?>" class="btn btn-secondary btn-sm mb-2" style="height:107px; width:100%;"  class="btn btn-primary btn-sm mb-2" style="height:85px; width:100%;">
                                                     <i class="bx bx-package bx-sm mb-1 mt-2"></i>
                                                     <p class="icon-name text-capitalize">Uji Mat Laser (QR Code)</p>
                                                 </a>
-                                            </div>
+                                            </div> -->
                                             <div class="col-4">
                                                 <a href="javascript:void(0)" onclick="changePageMain()" data-url="<?=base_url('index.php/ujimat_flash')?>" class="btn btn-secondary btn-sm mb-2" style="height:107px; width:100%;"  class="btn btn-primary btn-sm mb-2" style="height:85px; width:100%;">
                                                     <i class="bx bx-package bx-sm mb-1 mt-2"></i>
-                                                    <p class="icon-name text-capitalize">Uji Mat CAM - Flash (QR Code)</p>
+                                                    <p class="icon-name text-capitalize">Uji Mat (QR Code)</p>
+                                                </a>
+                                            </div>
+                                            <div class="col-4">
+                                                <a href="javascript:void(0)" onclick="changePageMain()" data-url="<?=base_url('index.php/scan_aset_v2')?>" class="btn btn-secondary btn-sm mb-2" style="height:107px; width:100%;">
+                                                    <i class="bx bx-notepad bx-sm mb-1 mt-2"></i>
+                                                    <p class="icon-name text-capitalize">Pindai Aset (QR Code)</p>
                                                 </a>
                                             </div>
                                             <div class="col-4">
@@ -134,19 +140,7 @@
                                                 </a>
                                             </div>
                                             <?php } ?>
-                                            <!-- <div class="col-4 align-items-center">
-                                                <a href="javascript:void(0)" onclick="changePageMain()" data-url="<?=base_url('index.php/ujimat_scanqr')?>" class="btn btn-primary btn-sm mb-2" style="height:107px; width:100%;">
-                                                    <i class="bx bx-search bx-sm mb-1 mt-2" style="font-size: 200px;"></i>
-                                                    <p class="icon-name text-capitalize">Uji Mat (QR Code)</p> -->
-                                                    <!-- <p class="icon-name text-capitalize">Uji Mat</p> -->
-                                                <!-- </a>
-                                            </div> -->
-                                            <div class="col-4">
-                                                <a href="javascript:void(0)" onclick="changePageMain()" data-url="<?=base_url('index.php/scan_aset_v2')?>" class="btn btn-secondary btn-sm mb-2" style="height:107px; width:100%;">
-                                                    <i class="bx bx-notepad bx-sm mb-1 mt-2"></i>
-                                                    <p class="icon-name text-capitalize">Pindai Aset (QR Code)</p>
-                                                </a>
-                                            </div>
+
                                         </div> <!-- end row -->
                                     </div>
                                 </div>
